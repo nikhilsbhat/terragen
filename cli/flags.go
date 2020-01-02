0adf91797cad38e32968a131936b4522351fc431
nikhil.bhat <ext_nbhat@falabella.cl>
2020-01-02 17:21:27 +0530

    Handling defaults better

--- cli/flags.go
+++ cli/flags.go
@@ -6,6 +6,6 @@ import (
 
 // Registering all the flags to the command neuron itself.
 func registerFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().StringVarP(&genin.Package, "name", "n", "", "name of the provider that needs templates")
-	cmd.PersistentFlags().StringVarP(&genin.Path, "path", "p", "", "path where the templates has to be generated")
+	cmd.PersistentFlags().StringVarP(&genin.Package, "name", "n", "demo", "name of the provider that needs templates")
+	cmd.PersistentFlags().StringVarP(&genin.Path, "path", "p", ".", "path where the templates has to be generated")
 }
